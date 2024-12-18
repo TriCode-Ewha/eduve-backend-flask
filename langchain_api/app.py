5520a4758fb26c8d00c1b6d62892391312fc9b18
hyejin2234 <hhj020304@ewhain.net>
2024-12-18 22:44:18 +0900

    purchase open api

--- langchain_api/app.py
+++ langchain_api/app.py
@@ -1,11 +1,17 @@
-""" from flask import Flask, jsonify, request
+'''
+from flask import Flask, jsonify, request
 import requests
+import openai
 
 app = Flask(__name__)
+'''
 
 # OpenAI API 키 설정
-openai.api_key = "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"  # 여기에 발급받은 API 키를 입력하세요
+# openai.api_key = "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"  # 여기에 발급받은 API 키를 입력하세요
+api_key = "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"  
+# api_key = "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"  
 
+'''
 @app.route('/chatgpt', methods=['POST'])
 def chatgpt():
     try:
@@ -28,22 +34,14 @@ def chatgpt():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run(debug=True, port=5000)
- """
+'''
 
-# API 키를 환경변수로 관리하기 위한 설정 파일
+
+
 from dotenv import load_dotenv
 
-# API 키 정보 로드
-load_dotenv()
-
-# LangSmith 추적을 설정합니다. https://smith.langchain.com
-# !pip install -qU langchain-teddynote
-from langchain_teddynote import logging
-
-# 프로젝트 이름을 입력합니다.
-logging.langsmith("langchain_api")
+load_dotenv()  # .env 파일에서 변수 로드
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyMuPDFLoader
@@ -51,12 +49,12 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_chroma import Chroma
-from langchain.vectorstores import Chroma
+from langchain_openai import  OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
 
 # 단계 1: 문서 로드(Load Documents)
 loader = PyMuPDFLoader("data/국세법령정보시스템.pdf")
+#loader = PyMuPDFLoader("data/투자론 과제_2276267 장예주.pdf")
 docs = loader.load()
 
 # 문서의 전체 페이지 수, 특정 페이지의 내용 확인용 출력
@@ -109,17 +107,24 @@ split_documents_with_page_numbers = split_with_page_numbers(docs, chunk_size=500
 # 분할된 청크의 수
 print(f"분할된 청크의 수: {len(split_documents_with_page_numbers)}")
 
-""" for idx, chunk in enumerate(split_documents_with_page_numbers):
-    print(f"청크 {idx+1}: {chunk}\n") """
+
+'''
+for idx, chunk in enumerate(split_documents_with_page_numbers):
+    print(f"청크 {idx+1}: {chunk}\n")
+'''
+
 
 # 단계 3: 임베딩(Embedding) 생성
-embeddings = OpenAIEmbeddings()
+#embeddings = OpenAIEmbeddings()
+embeddings = OpenAIEmbeddings(openai_api_key=api_key)
 
-""" # 단계 4: 전체 문서 임베딩
+'''
+# 단계 4: 전체 문서 임베딩
 embeddings_list = embeddings.embed_documents(split_documents_with_page_numbers)
 
 # 임베딩이 잘 되었는지 첫번째 청크의 벡터값 확인용 출력
-print("첫 번째 청크 벡터:", embeddings_list[0]) """
+print("첫 번째 청크 벡터:", embeddings_list[0])
+'''
 
 contents = [doc["content"] for doc in split_documents_with_page_numbers]
 metadatas = [doc["metadata"] for doc in split_documents_with_page_numbers]
@@ -138,11 +143,13 @@ for result in results:
 # Chroma로 데이터 저장 및 로드
 vectorstore = Chroma.from_texts(texts=contents, embedding=embeddings, metadatas=metadatas)
 
+'''
 # 모든 데이터 출력
 for doc in vectorstore.similarity_search("", k=len(contents)):
     print(f"내용: {doc.page_content}")
     print(f"메타데이터: {doc.metadata}")
     print()
+'''
 
 # 유사도검색1
 docs1 = vectorstore.similarity_search("양도소득세", k=2)
@@ -219,3 +226,4 @@ retriever = db.as_retriever(
     search_kwargs={"filter": {"source": "data/finance-keywords.txt"}, "k": 2}
 )
 retriever.invoke("ESG 에 대하여 알려줘") """
+
